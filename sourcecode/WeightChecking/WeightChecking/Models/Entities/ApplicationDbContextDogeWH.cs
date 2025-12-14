9e8b5bdefe22fb9370cc19bbf53bfd9a0417cad4
congnguyen-framas <68525123+congnguyen-framas@users.noreply.github.com>
2025-12-14 19:54:55 +0700

    update OK

---
+++ sourcecode/WeightChecking/WeightChecking/Models/Entities/ApplicationDbContextDogeWH.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightChecking.Models.Entities
+{
+    public class ApplicationDbContextDogeWH : DbContext
+    {
+        public ApplicationDbContextDogeWH(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
+        protected ApplicationDbContextDogeWH()
+        {
+        }
+    }
+}
---
+++ sourcecode/WeightChecking/WeightChecking/Models/Entities/ApplicationDbContextWL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightChecking.Models.Entities
+{
+    public class ApplicationDbContextWL:DbContext
+    {
+        public ApplicationDbContextWL(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
+        protected ApplicationDbContextWL()
+        {
+        }
+    }
+}
