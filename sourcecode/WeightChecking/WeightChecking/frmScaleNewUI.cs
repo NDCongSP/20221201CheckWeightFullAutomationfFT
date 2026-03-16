ff72f5bf91bdec0ac6f36e2531a1a196f29e6392
Cong Nguyen <68525123+congnguyen-framas@users.noreply.github.com>
2026-03-16 09:06:23 +0700

    update

--- sourcecode/WeightChecking/WeightChecking/frmScaleNewUI.cs
+++ sourcecode/WeightChecking/WeightChecking/frmScaleNewUI.cs
@@ -3,6 +3,7 @@ using CognexLibrary_NETFramework;
 using CoreScanner;
 using Dapper;
 using DevExpress.XtraEditors;
+using DevExpress.XtraRichEdit.Import.Html;
 using DevExpress.XtraSplashScreen;
 using DevExpress.XtraSpreadsheet.Model;
 using Newtonsoft.Json;
@@ -327,7 +328,7 @@ namespace WeightChecking
         }
 
 
-        private void FrmScale_Load(object sender, EventArgs e)
+        private async void FrmScale_Load(object sender, EventArgs e)
         {
 
             using var dbContext = new ApplicationDbContextSSFG(GlobalVariables.ConnectionString);
@@ -701,6 +702,64 @@ namespace WeightChecking
                 }
                 #endregion
 
+                #region Connect to PL Seimens
+                _manager.LoadFromConfig("tags.json");
+                //_manager.LoadFromConfig("tags1.json");
+
+                //tùy vào hệ thống kết nối bao nhiêu PLC để gọi kết nối đến PLC tương ứng
+                _plcRuntime = _manager.GetPlc("PLC_1");
+                _plc1Client = _plcRuntime.Client; // Lưu vào biến toàn cục
+
+                // ĐĂNG KÝ SỰ KIỆN TRƯỚC KHI KẾT NỐI
+                _plc1Client.StateChanged += Client_StateChanged;
+
+                await _plcRuntime.Reader.ReadGroupAsync(_plcRuntime.Tags);
+
+
+                // 1) Tạo và gán handler
+                _sub = new PlcSubscriptionManager(_plcRuntime.Reader);
+                _sub.OnValueChanged += Sub_OnValueChanged;
+
+                //Đăng ký tag value chcange cho từng tag
+                //_plcRuntime.Tags.FirstOrDefault(t => t.Name == "Scale_Stable_Trigger").ValueChanged += (tag) =>
+                //{
+                //    Debug.WriteLine($"{DateTime.Now:O} [{tag.Name}] {tag.LastValue} -> {tag.NewValue} ({tag.DataType}) -> Deadband:{tag.Deadband}");
+
+                //    if (tag.NewValue.ToString() == "1")
+                //    {
+                //        _triggerInspectionMetal.Set();
+                //    }
+                //};
+
+                _plcRuntime.Tags.FirstOrDefault(t => t.Name == "Sccale_Value").ValueChanged += (tag) =>
+                {
+                    Debug.WriteLine($"{DateTime.Now:O} [{tag.Name}] {tag.LastValue} -> {tag.NewValue} ({tag.DataType}) -> Deadband:{tag.Deadband}");
+
+                    //if (tag.NewValue.ToString() == "1")
+                    //{
+                    //    _triggerInspectionWeight.Set();
+                    //}
+
+                    _scaleValue = (double)tag.NewValue;
+
+                    GlobalVariables.InvokeIfRequired(this, () =>
+                    {
+                        labScaleValue.Text = _scaleValue.ToString();
+                    });
+                };
+
+                // 2) Kết nối PLC, chạy Polling
+                await _plc1Client.ConnectAsync();
+                _plc1Client.StartWatchdog(2000);
+
+                // 3) (Tuỳ chọn) cập nhật UI lần đầu
+                foreach (var tag in _plcRuntime.Tags)
+                    tag.RaiseValueChanged();//Tự "bắn" sự kiện để UI cập nhật ngay giá trị ban đầu
+
+                // 4) BẮT ĐẦU POLLING (rất quan trọng)
+                _sub.Subscribe(_plcRuntime.Tags, intervalMs: 200);
+                #endregion
+
                 #region Ket noi conveyor
                 GlobalVariables.ConveyorStatus = GlobalVariables.MyDriver.S7Ethernet.Client.KetNoi(GlobalVariables.ConfigJson.IpConveyor);
                 //GlobalVariables.ConveyorStatus = GlobalVariables.MyDriver.S7Ethernet.Client.KetNoi("10.40.0.112");
@@ -857,6 +916,19 @@ namespace WeightChecking
             ResetControl();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        private void Sub_OnValueChanged(PlcTag obj)
+        {
+            Debug.WriteLine($"{obj.Name} = {obj.NewValue}");
+        }
+
+        private void Client_StateChanged(PlcConnectionState obj)
+        {
+            _plcConnectionState = obj;
+        }
         private void DataEvent_EventHandleStatusChange(object sender, StatusChangeEventArgs e)
         {
             GlobalVariables.CognexCam_2Status = e.Status;
@@ -3611,6 +3683,8 @@ namespace WeightChecking
                 goto Loop1;
             }
         }
+
+
         #endregion
 
         private void ShowUI(bool errorFlag = false)
