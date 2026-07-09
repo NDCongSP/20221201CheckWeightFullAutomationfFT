8741183a40e77e1e5192c282ee65bfba06d81376
Cong Nguyen <68525123+congnguyen-framas@users.noreply.github.com>
2026-07-09 17:59:15 +0700

    Refactor solution and update label printing logic
    Removed unused projects `S7Server` and `S7Client` from the solution.
    Added the `ZebraSDKTest` project with a new directory structure.
    Replaced `snap7.net` reference with `Snap7Scada.Lib` in `WeightChecking.csproj`.
    Updated `SendDynamicString` in `frmScaleNewUI.cs` to remove date info
    from labels by setting `createdDate` to a blank string.

--- sourcecode/WeightChecking/WeightChecking/frmScaleNewUI.cs
+++ sourcecode/WeightChecking/WeightChecking/frmScaleNewUI.cs
@@ -3038,6 +3038,9 @@ namespace WeightChecking
         {
             try
             {
+                //hiện tại yêu cầu bỏ hết thông tin ngày tháng khi in tem.
+                createdDate = " ";
+
                 int i = 0, j = 0, k = 0;
                 int chkSUM = 0;
 
